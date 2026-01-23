d634efc33fe1aa426d0a46a5b3a6818d1f643c6e
Michael de Vera <mldevera@amazon.com>
2026-01-23 22:17:16 +0000

    Fixing merge issue

--- jdk/src/share/bin/splashscreen_stubs.c
+++ jdk/src/share/bin/splashscreen_stubs.c
@@ -60,11 +60,11 @@ typedef char* (*SplashGetScaledImageName_t)(const char* fileName,
 #define INVOKE(name,def) _INVOKE(name,def,return)
 #define INVOKEV(name) _INVOKE(name, ,;)
 
-int     DoSplashLoadMemory(void* pdata, int size) {
+void     DoSplashLoadMemory(void* pdata, int size) {
     INVOKE(SplashLoadMemory, 0)(pdata, size);
 }
 
-int     DoSplashLoadFile(const char* filename) {
+void     DoSplashLoadFile(const char* filename) {
     INVOKE(SplashLoadFile, 0)(filename);
 }
 
